8d6ba84475d991113e7aa88cbba49b1c70d1a753
aniyasinghaniya <105536074+aniyasinghaniya@users.noreply.github.com>
2023-07-18 18:53:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-img="";
+ img="";
 status="";
 object=[];
 
@@ -6,6 +6,17 @@ object=[];
 
 function draw(){
   image(img,0,0,400,300);
+  if (status != ""){
+    for(i=0 ; i<object.length;i++){
+      document.getElementById("status").innerHTML="Object-detected";
+      fill("blue");
+      percent=Math.floor(object[i].confidence*100);
+      text(object[i].label+" "+percent + "%",object[i].x +15,object[i].y +15);
+      noFill( );
+      stroke("blue")
+      rect(object[i].x,object[i].y,object[i].width,object[i].height)
+    }
+  }
 }
 function setup(){
  canvas=createCanvas(400,300);
@@ -23,6 +34,14 @@ function modelLoaded(){
     status=true;
     objectDetector.detect(img,gotResult)
 }
-function gotResult(){
-    
+function gotResult(error,results){
+  if(error){
+    console.log(error);
+  }
+    else{
+      console.log(results)
+      object=results
+    }
+
+
 }
